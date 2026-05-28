ccde6b2e8f25de3818c66f357c9c318d10e592e8
0xKoller <joseluiskoller98@gmail.com>
2026-05-27 21:04:45 -0300

    feat: add local landing.html (early distribution page) + improved vite config + package scripts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import preact from '@preact/preset-vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  build: {
+    // We will produce a single-file friendly bundle for console later
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
